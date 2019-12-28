dbb8c546f550e478e1a5483f980144c4e483db35
ameba23 <ameba23@systemli.org>
2019-12-28 07:07:41 +0100

    readme

--- extractors/music-metadata.js
+++ extractors/music-metadata.js
@@ -4,10 +4,20 @@ const mm = require('music-metadata')
 
 // TODO: can add 'duration:true' but calculating it is expensive (whole file must be parsed)
 // skipCovers: true stops covers being extracted
-module.exports = function (data, { mimetype }, callback) {
-  mm.parseBuffer(data, mimetype, { skipCovers: true })
+module.exports = function (data, input, callback) {
+  const mimeType = input.mimeType
+  if (mimeType) {
+    if (mimeType.split('/')[0] !== 'audio') return callback()
+  } else {
+    // TODO mimetype from extension
+    if (!['mp3', 'flac'].includes(input.extension)) return callback()
+  }
+  mm.parseBuffer(data, mimeType, { skipCovers: true })
     .then((metadata) => {
       // console.log(util.inspect(metadata, { showHidden: false, depth: null }));
       callback(null, { 'music-metadata': metadata })
+    }).catch((err) => {
+      console.log(err)
+      callback()
     })
 }
