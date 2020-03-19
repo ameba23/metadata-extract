f07d5bd59cf67f3ef241b64c4564046e059ea042
ameba23 <ameba23@systemli.org>
2020-03-19 18:21:16 +0100

    use streams

--- extractors/text.js
+++ extractors/text.js
@@ -4,12 +4,13 @@ const MAXBYTES = 1024
 module.exports = function (data, input, callback) {
   const mimeType = input.mimeType
   if (mimeType) {
-    if (mimeType.split('/')[0] !== 'text') return callback()
+    if ((mimeType.split('/')[0] !== 'text') && (mimeType !== 'audio/x-mpegurl')) return callback()
   }
   let previewText = ''
-  const rl = readline.createInterface({
-    input: data // stream
-  })
+  // const rl = readline.createInterface({input: data // stream})
+
+
+  const rl = readline.createInterface(data)
   rl.on('line', (line) => {
     previewText += `${line}\n`
     if (Buffer.from(previewText).length > MAXBYTES) {
--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const assert = require('assert')
 
 const extractorsPath = './extractors/'
 const defaultExtractors = ['music-metadata']
+// const defaultExtractors = ['music-metadata', 'text']
 
-module.exports = function extract (data, opts = {}, callback) {
+module.exports = function extract (dataStream, opts = {}, callback) {
   if (typeof opts === 'function' && !callback) {
     callback = opts
     opts = {}
@@ -24,66 +25,69 @@ module.exports = function extract (data, opts = {}, callback) {
   })
 
   if (opts.filename) metadata.extension = path.extname(opts.filename)
-  metadata.mimeType = getMimeType(data, metadata.extension)
-  log('mimetype: ', metadata.mimeType)
+  // metadata.mimeType = getMimeType(dataStream, metadata.extension)
+  getMimeType(dataStream, metadata.extension, (err, mimeType) => {
+    if (err) mimeType = undefined
+    metadata.mimeType = mimeType
 
-  pull(
-    pull.values(extractors),
-    pull.asyncMap((extractor, cb) => {
-      try {
-        extractor(data, metadata, cb)
-      } catch (err) {
-        log('Error from extractor: ', extractor.name, err)
-        cb(null, data) // ignore errors and keep going
-      }
-    }),
-    // pull.filter(Boolean),
-    pull.filter(t => !!t),
-    pull.map(sanitise),
-    // should this be a reducer?
-    pull.collect((err, metadatas) => {
-      if (err) return callback(err) // TODO: or ingore?
-      Object.assign(metadata, ...metadatas)
-      callback(null, metadata)
-    })
-  )
+    log('mimetype: ', metadata.mimeType)
 
-  function sanitise (metadata) {
-    if (metadata && typeof metadata === 'object') {
-      Object.keys(metadata).forEach((key) => {
-        const value = metadata[key]
-        if (!value) {
-          delete metadata[key]
-          return
+    pull(
+      pull.values(extractors),
+      pull.asyncMap((extractor, cb) => {
+        try {
+          extractor(dataStream, metadata, cb)
+        } catch (err) {
+          log('Error from extractor: ', extractor.name, err)
+          cb(null, {}) // ignore errors and keep going
         }
-        if (typeof value === 'object') {
-          if (isEmptyObject(value) || isEmptyArray(value)) {
-            delete metadata[key]
-          } else {
-            metadata[key] = sanitise(value)
-            if (isEmptyObject(metadata[key]) || isEmptyArray(metadata[key])) delete metadata[key]
-          }
-        }
-        // Dont allow buffers
-        if (Buffer.isBuffer(value) && !opts.allowBuffers) delete metadata[key]
+      }),
+      // pull.filter(Boolean),
+      pull.filter(t => !!t),
+      pull.map(sanitise),
+      // should this be a reducer?
+      pull.collect((err, metadatas) => {
+        if (err) return callback(err) // TODO: or ingore?
+        Object.assign(metadata, ...metadatas)
+        callback(null, metadata)
       })
+    )
+
+    function sanitise (metadata) {
+      if (metadata && typeof metadata === 'object') {
+        Object.keys(metadata).forEach((key) => {
+          const value = metadata[key]
+          if (!value) {
+            delete metadata[key]
+            return
+          }
+          if (typeof value === 'object') {
+            if (isEmptyObject(value) || isEmptyArray(value)) {
+              delete metadata[key]
+            } else {
+              metadata[key] = sanitise(value)
+              if (isEmptyObject(metadata[key]) || isEmptyArray(metadata[key])) delete metadata[key]
+            }
+          }
+          // Dont allow buffers
+          if (Buffer.isBuffer(value) && !opts.allowBuffers) delete metadata[key]
+        })
+      }
+      return metadata
     }
-    return metadata
-  }
+  })
 }
 
-function getMimeType (data, extension) {
+function getMimeType (dataStream, extension, callback) {
   // TODO: file-type can also take a stream
-  let ft
-  if (data.length >= fileType.minimumBytes) {
-    ft = fileType(data)
-  }
-
-  // if we cannot determine mime type from data,
-  // use the extension. (this is less reliable)
-  return ft
-    ? ft.mime
-    : extension ? mime.getType(extension) : undefined
+  fileType.stream(dataStream).then((fileTypeObject) => {
+    // if we cannot determine mime type from data,
+    // use the extension. (this is less reliable)
+    const fileType = fileTypeObject.fileType
+      ? fileTypeObject.fileType.mime
+      : extension ? mime.getType(extension) : undefined
+    callback(null, fileType)
+  }).catch(callback)
 }
 
 function isEmptyObject (thing) {
