72d0ca3f13be620211ef30430b0ff58d6acc7496
ameba23 <ameba23@systemli.org>
2020-08-06 15:32:10 +0200

    exclude djvu

--- extractors/image-size.js
+++ extractors/image-size.js
@@ -5,8 +5,7 @@ const sizeOf = require('image-size')
 // same thing. but this is pure js (no external deps)
 
 module.exports = function (filename, { mimeType }, callback) {
-  // TODO: try catch ?
-  callback(null, (mimeType && mimeType.split('/')[0] === 'image')
+  callback(null, (mimeType && mimeType.split('/')[0] === 'image' && mimeType !== 'image/vnd.djvu')
     ? { imageSize: sizeOf(filename) }
     : null
   )
