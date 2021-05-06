81943a3b214a6a88c44d922c6ab4900370de91f6
ameba23 <ameba23@systemli.org>
2021-05-06 10:54:23 +0200

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,22 @@
 const test = require('tape')
 const extract = require('..')
-const pull = require('pull-stream')
 const fs = require('fs')
 const path = require('path')
 const assets = path.join(__dirname, 'test-media')
 
-test('index a directory', t => {
-  fs.readdir(assets, (err, files) => {
-    t.error(err, 'No error on reading test media directory')
-    pull(
-      pull.values(files),
-      pull.asyncMap((file, cb) => {
-        const filename = path.join(assets, file)
-        extract(filename, (err, metadata) => {
-          console.log(metadata)
-          t.error(err, `No error on extracting from ${file}`)
-          cb(null, metadata)
-        })
-      }),
-      pull.collect((err) => {
-        t.error(err, 'No error from reading all files')
-        t.end()
-      })
-    )
+test('index a directory', async t => {
+  const files = await new Promise((resolve, reject) => {
+    fs.readdir(assets, (err, files) => {
+      if (err) return reject(err)
+      resolve(files)
+    })
   })
+  console.log(files)
+  for (const file of files) {
+    const filename = path.join(assets, file)
+    const metadata = await extract(filename)
+    console.log(metadata)
+    t.ok(metadata, `No error on extracting from ${file}`)
+  }
+  t.end()
 })
